d9ed20ccbc640c301191582b7bf2b20c500a2e89
Reilly <reillymarkowitz@gmail.com>
2019-02-08 17:19:36 -0600

    Move API logic to server.js

--- app/server.js
+++ app/server.js
@@ -4,4 +4,8 @@ const app = express()
 const port = 3000
 
 app.use(express.static('public'))
+
+var priceRouter = require('./routes/price')
+app.use('/price', priceRouter);
+
 app.listen(port, () => console.log(`App listening on port ${port}!`))
