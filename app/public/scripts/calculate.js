e66804b395d0c73433dd7859bdbd37db5efb7e27
Reilly <reillymarkowitz@gmail.com>
2019-02-08 18:36:32 -0600

    test years since

--- app/public/scripts/calculate.js
+++ app/public/scripts/calculate.js
@@ -92,8 +92,8 @@ function formatUSD(num) {
 
 function reward(date) {
     epoch = new Date(2019, 0, 1)
-    yearsSince = date.getFullYear - epoch.getFullYear
+    yearsSince = date.getFullYear() - epoch.getFullYear()
     reward = 8.6625 * Math.pow((1 - 0.0833), yearsSince)
-    console.log(Math.pow(3, 2))
+    console.log(yearsSince)
     return reward
 }
